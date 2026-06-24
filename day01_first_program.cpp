4d783ae4e93c4ec811137979d4361a9bf84d7839
Amr Khaled <amrk07401@mail.com>
2026-06-24 20:14:53 +0300

    Day 1: First C++ program

---
+++ day01_first_program.cpp
@@ -0,0 +1,9 @@
+#include<iostream> //we are demanding the processor to include the iostream header file which contains the definition of the std::cout object and the std::endl manipulator.
+
+// the main function is the entry point of the program. It is where the execution of the program begins.
+int main()
+//this is the body of the main function. It contains the statements that will be executed when the program runs.
+{
+    std::cout<<"hello from amr "<<std::endl; //this is the program that will run in the main function , endl is used to either print a new line or flush the output buffer.
+    return 0; //this means that the program has executed successfully and is returning a value of 0 to the operating system.
+}
